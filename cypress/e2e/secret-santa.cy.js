fa03756dd59fd4521d7f9fe9f8f1880f24c76479
nataliasiutsova <tashaklimenkova@gmail.com>
2023-03-05 11:17:34 +0300

    Deleted cypress test examples

--- cypress/e2e/secret-santa.cy.js
+++ cypress/e2e/secret-santa.cy.js
@@ -18,15 +18,3 @@ describe('Santa-secret UI testing', () => {
     cy.url().should('eq', 'https://santa-secret.ru/login');
   });
 });
-
-//   it('displays home page buttoms', () => {
-//     cy.get('.home-page-buttons a')
-//       .contains('Создать коробку')
-//       .should('have.attr', 'href');
-//     cy.get('[href="/login"]').click();
-//     cy.get('.home-page-buttons a:last-child')
-//       .contains('Быстрая жеребьевка')
-//       .should('have.attr', 'href');
-//     cy.get('[href="/randomizer"]').click();
-//   });
-// });
